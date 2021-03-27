1f4ba644744407907edfdac85c6cff080e84a639
Shyam <shyam10kwd@gmail.com>
2021-03-27 18:39:41 +0530

    Rolles Back CLasses

--- dolwnloadSorter.py
+++ dolwnloadSorter.py
@@ -20,17 +20,14 @@ class sorter:
         self.path = path
         os.chdir(path)
 
-
-        while True:
-            items = items = os.listdir()
-            for item in items:
-                f_name , f_ext = os.path.splitext(item)
-                if f_ext in photos:
-                    os.renames(item, 'Phtotos/'+item)
-                elif f_ext in pdf:
-                    os.renames(item, 'Documents/'+item)
-            
-            time.sleep(1800)
+        items = os.listdir()
+        print(items)
+        for item in items:
+            f_name , f_ext = os.path.splitext(item)
+            if f_ext in photos:
+                os.renames(item, 'Sort_Photos/'+item)
+            elif f_ext in pdf:
+                os.renames(item, 'Sort_Docs/'+item)
 
 if __name__ == '__main__':
-    sorter('/home/shyam/Downloads')
+    sorter('/run/user/1000/gvfs/google-drive:host=gmail.com,user=shyam10kwd')
