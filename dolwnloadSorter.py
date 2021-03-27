48ddd369132e2a2bc53ba2b85e2fc731338bd3a7
Shyam <shyam10kwd@gmail.com>
2021-03-27 09:18:02 +0530

    make classes

--- dolwnloadSorter.py
+++ dolwnloadSorter.py
@@ -7,22 +7,30 @@ in background and checks the download folder every thirty miniutes.
 
 '''
 
-photos = ['.png', '.jpg', '.jpeg', '.svg']
-pdf = ['.pdf', 'docs']
-archieve = ['.tar', '.xz','.deb']
-
-path = '/home/shyam/Downloads'
-
-os.chdir(path)
 
 
-while True:
-    items = items = os.listdir()
-    for item in items:
-        f_name , f_ext = os.path.splitext(item)
-        if f_ext in photos:
-            os.renames(item, 'Phtotos/'+item)
-        elif f_ext in pdf:
-            os.renames(item, 'Documents/'+item)
-    
-    time.sleep(1800)
+class sorter:
+
+    def __init__(self, path):
+
+        photos = ['.png', '.jpg', '.jpeg', '.svg']
+        pdf = ['.pdf', 'docs']
+        archieve = ['.tar', '.xz','.deb']
+
+        self.path = path
+        os.chdir(path)
+
+
+        while True:
+            items = items = os.listdir()
+            for item in items:
+                f_name , f_ext = os.path.splitext(item)
+                if f_ext in photos:
+                    os.renames(item, 'Phtotos/'+item)
+                elif f_ext in pdf:
+                    os.renames(item, 'Documents/'+item)
+            
+            time.sleep(1800)
+
+if __name__ == '__main__':
+    sorter('/home/shyam/Downloads')
